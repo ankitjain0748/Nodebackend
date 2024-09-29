3962076924c67a52ed088195a7a886b0e346b223
ankitjain0748 <ankitjain0748@gmail.com>
2024-09-29 23:47:01 +0530

    manage user

---
+++ Controller/UserController.js
@@ -0,0 +1,31 @@
+const User = require('../models/signup');
+const { successResponse, errorResponse, validationErrorResponse } = require('../helper/responseHelper');
+
+const signup = async (req, res) => {
+    const { name, mpin, phone, username } = req.body;
+    if (!name || !mpin || !phone || !username) {
+        return validationErrorResponse(res, { name: 'Name is required', mpin: 'MPIN is required', phone: 'Phone is required', username: 'Username is required' });
+    }
+
+    try {
+        const existingUser = await User.findOne({ username });
+        if (existingUser) {
+            return validationErrorResponse(res, { username: 'Username already exists' });
+        }
+        const newUser = new User({
+            name,
+            mpin,
+            phone,
+            username,
+        });
+        await newUser.save();
+        return successResponse(res, newUser, "User created successfully");
+    } catch (error) {
+        console.error(error);
+        return errorResponse(res, "Error creating user");
+    }
+};
+
+module.exports = {
+    signup,
+};
